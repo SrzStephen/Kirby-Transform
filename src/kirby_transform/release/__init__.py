277fc86ea230b3de047031a29b0d6974d2e252e1
Stephen Mott <stephen@motts.id.au>
2020-12-20 16:54:00 +0800

    adds deploy sanity checks before pypi push

---
+++ src/kirby_transform/release/__init__.py
@@ -0,0 +1,1 @@
+from kirby_transform.release.helpers import GitHelpers, VersionHelper, PIPI, DEV_PIPI
---
+++ src/kirby_transform/release/helpers.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from pygit2 import Repository
+from kirby_transform import __version__
+from johnnydep import JohnnyDist
+from pip._internal.exceptions import DistributionNotFound
+from subprocess import CalledProcessError
+from os import popen
+import click
+
+# Definitions because I can't remember that
+DEV_PIPI = 'https://test-files.pythonhosted.org'
+PIPI = 'https://files.pythonhosted.org'
+
+
+class GitHelpers(object):
+    @staticmethod
+    def get_branch_from_repo(repo_path: Path) -> str:
+        if repo_path.is_file():
+            raise ValueError("Expected path to a repo with .git at root")
+        repo = Repository(str(repo_path))
+        full_repo_name = repo.head.name
+        return full_repo_name[len('refs/heads/'):]
+
+    @staticmethod
+    def get_current_branch() -> str:
+        return popen('git branch --show-current').read().replace('\n', '')
+
+
+class VersionHelper(object):
+
+    @staticmethod
+    def kirb_transform_version_in_repo(index_url=PIPI) -> bool:
+        try:
+            return __version__ in JohnnyDist(req_string=__package__, index_url=index_url)
+        # Get these two errors if the dist doesn't exist
+        except (CalledProcessError, DistributionNotFound) as e:
+            return False
+
+    @staticmethod
+    def is_using_latest_kirb_transform(index_url=PIPI) -> bool:
+        kirb = JohnnyDist(req_string=__package__, index_url=index_url)
+        return kirb.version_installed == kirb.version_latest
+
+    @staticmethod
+    def version_in_pipi(package_name: str, version: str, pipi_repo: str = PIPI) -> bool:
+        return version in JohnnyDist(req_string=package_name, index_url=pipi_repo).versions_available
